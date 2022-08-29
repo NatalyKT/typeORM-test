46bda96d6382721f986673a8e4af08e9fd69239f
nataly_kt <nataly_kt@hotmail.com>
2022-08-29 20:47:25 +0300

    Creating and saving a one-to-one relation

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,40 @@
 import { AppDataSource } from "./data-source"
 import { Photo } from "./entity/Photo"
+import { PhotoMetadata } from "./entity/PhotoMetadata"
 
 AppDataSource.initialize()
     .then(async () => {
 
+        // create a photo
+        const photo = new Photo()
+        photo.name = "Me and Bears"
+        photo.description = "I am near polar bears"
+        photo.filename = "photo-with-bears.jpg"
+        photo.views = 1
+        photo.isPublished = true
+
+        // create a photo metadata
+        const metadata = new PhotoMetadata()
+        metadata.height = 640
+        metadata.width = 480
+        metadata.compressed = true
+        metadata.comment = "cybershoot"
+        metadata.orientation = "portrait"
+        metadata.photo = photo // this way we connect them
+
+        // get entity repositories
         const photoRepository = AppDataSource.getRepository(Photo)
+        const metadataRepository = AppDataSource.getRepository(PhotoMetadata)
 
-        const photoToUpdate = await photoRepository.findOneBy({
-            id: 1,
-        })
-        photoToUpdate.name = "Me, my friends and polar bears"
-        await photoRepository.save(photoToUpdate)
+        // first we should save a photo
+        await photoRepository.save(photo)
 
-        const photoToRemove = await photoRepository.findOneBy({
-            id: 3,
-        })
-        await photoRepository.remove(photoToRemove)
+        // photo is saved. Now we need to save a photo metadata
+        await metadataRepository.save(metadata)
 
-        const allPhotos = await photoRepository.find()
-        console.log("All photos from the db: ", allPhotos)
-
-        const firstPhoto = await photoRepository.findOneBy({
-            id: 1,
-        })
-        console.log("First photo from the db: ", firstPhoto)
-
-        const meAndBearsPhoto = await photoRepository.findOneBy({
-            name: "Me and Bears",
-        })
-        console.log("Me and Bears photo from the db: ", meAndBearsPhoto)
-
-        const allViewedPhotos = await photoRepository.findBy({
-            views: 1
-        })
-        console.log("All viewed photos: ", allViewedPhotos)
-
-        const allPublishedPhotos = await photoRepository.findBy({
-            isPublished: true
-        })
-        console.log("All published photos: ", allPublishedPhotos)
-
-        const [photos, photosCount] = await photoRepository.findAndCount()
-        console.log("All photos: ", photos)
-        console.log("Photos count: ", photosCount)
-
+        // done
+        console.log(
+            "Metadata is saved, and the relation between metadata and photo is created in the database too",
+        )
     })
     .catch((error) => console.log(error))
