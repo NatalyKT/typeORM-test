9409b0f45da3deade296b294dc855ab931b94a9c
nataly_kt <nataly_kt@hotmail.com>
2022-08-30 00:58:05 +0300

    Refactoring: add data to the database

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,39 @@
 import { AppDataSource } from "./data-source"
 import { Photo } from "./entity/Photo"
 import { PhotoMetadata } from "./entity/PhotoMetadata"
+import { Album } from "./entity/Album"
 
 AppDataSource.initialize()
     .then(async () => {
-/*
+
+        const album1 = new Album()
+        album1.name = "Bears"
+        await AppDataSource.manager.save(album1)
+
+        const album2 = new Album()
+        album2.name = "Me"
+        await AppDataSource.manager.save(album2)
+
         const photo = new Photo()
         photo.name = "Me and Bears"
         photo.description = "I am near polar bears"
         photo.filename = "photo-with-bears.jpg"
+        photo.views = 1
         photo.isPublished = true
+        photo.albums = [album1, album2]
+        await AppDataSource.manager.save(photo)
 
-        const metadata = new PhotoMetadata()
-        metadata.height = 640
-        metadata.width = 480
-        metadata.compressed = true
-        metadata.comment = "cybershoot"
-        metadata.orientation = "portrait"
+        const loadedPhoto = await AppDataSource.getRepository(Photo).findOne({
+            where: {
+                id: 1,
+            },
+            relations: {
+                albums: true,
+            },
+        })
 
-        photo.metadata = metadata // this way we connect them
+        console.log("Photo is saved, photo & album data is saved too, ", loadedPhoto)
 
-        const photoRepository = AppDataSource.getRepository(Photo)
-
-        await photoRepository.save(photo)
-
-        console.log("Photo is saved, photo metadata is saved too.")*/
+        
     })
     .catch((error) => console.log(error))
